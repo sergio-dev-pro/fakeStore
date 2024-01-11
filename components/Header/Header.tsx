6dcbfd5265d2268a58891be26233d8d56e8a9e05
sergio-dev-pro <sergiolucasoliveira02@gmail.com>
2024-01-11 11:15:25 -0300

    Add header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { Dropdown } from "../Dropdown";
+import { Logo } from "../Logo";
+import "./header.scss";
+import { Button } from "../Button";
+import { Link } from "../Link";
+type Props = {};
+
+export const Header = (props: Props) => {
+  return (
+    <header>
+      <Logo />
+      <Dropdown>
+        <Dropdown.Target>Minha conta</Dropdown.Target>
+        <Dropdown.Content>
+          <Button style="secondary" className="dropdown__content__button">
+            <Link href="/signin">Fazer login</Link>
+          </Button>
+          <Button style="secondary" className="dropdown__content__button">
+            <Link href="/new-account">Criar conta</Link>
+          </Button>
+        </Dropdown.Content>
+      </Dropdown>
+    </header>
+  );
+};
