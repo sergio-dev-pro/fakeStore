4155aa54f48a4656e6b7e5a79aeb3b815df05eb4
sergio-dev-pro <sergiolucasoliveira02@gmail.com>
2024-01-18 16:02:06 -0300

    refactor globalState lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.scss";
-import StoreProvider from "./StoreProvider";
+import StoreProvider from "../lib/globalState/StoreProvider";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 // FontAwesome bug solution https://stackoverflow.com/questions/56334381/why-my-font-awesome-icons-are-being-displayed-big-at-first-and-then-updated-to-t
