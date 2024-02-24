a7d159a727a67864ef746423493859c340c381aa
sergio-dev-pro <sergiolucasoliveira02@gmail.com>
2024-02-24 19:11:23 -0300

    fix

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -15,16 +15,19 @@ export default async function Home() {
     <section>
       {products.map((product) => (
         <div className="product-card" key={product.id}>
-          <div className="product-card__img">
-            <Image fill src={product.image} alt={product.title} />
+          <div className="product-card__grid-row-1">
+            <div className="product-card__img">
+              <Image fill src={product.image} alt={product.title} />
+            </div>
           </div>
-          <div>
+
+          <div className="product-card__grid-row-2">
             <h1 className="product-card__title">{product.title}</h1>
             <div>
               <span className="product-card__price">
                 {formatNumberToCurrency(product.price)}
               </span>
-              <Button style="secondary">+</Button>
+              <Button style="bluish-green-secondary">+</Button>
             </div>
           </div>
         </div>
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,11 @@
 import React, { ButtonHTMLAttributes, ReactNode } from "react";
 import "./button.scss";
 
-export type buttonStyles = "primary" | "secondary" | "secondary-blue";
+export type buttonStyles =
+  | "primary"
+  | "secondary"
+  | "secondary-blue"
+  | "bluish-green-secondary";
 type Props = {
   children: ReactNode;
   style?: buttonStyles;
